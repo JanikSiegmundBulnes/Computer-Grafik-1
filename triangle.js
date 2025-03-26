f45dc27869dd5e670aeca60c8e88055ab0ea66fa
Janik Siegmund Bulnes <janik.siegmundbulnes@gmail.com>
2025-03-26 10:38:53 +0100

    color as hex fix

--- triangle.js
+++ triangle.js
@@ -2,37 +2,44 @@
 
 async function init() {
  console.log('Hallo');
+
+ // "canvas" Element aus der HTML holen
  let canvas = document.getElementById('cg1-canvas');
  let gl = canvas.getContext('webgl');
  if(!gl) {
     console.error('webgl not supported');
  }
 
- // create shader program
+ // Vertex Shader code aus externer Datei laden
+  // Vertex Shader fuer Berechnung der Position eines Punktes verantwortlich
  let vertexTextResponse = await fetch('./vertexShader.glsl');
  let vertexText = await vertexTextResponse.text();
 
- let vertexShader = gl.createShader(gl.VERTEX_SHADER);
- gl.shaderSource(vertexShader, vertexText);
- gl.compileShader(vertexShader);
- if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+ let vertexShader = gl.createShader(gl.VERTEX_SHADER); // Vertex Shader erstellen
+ gl.shaderSource(vertexShader, vertexText); // Shader Code in WebGL laden
+ gl.compileShader(vertexShader); // Shader kompilieren
+ if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){ // pruefen ob Fehler beim kompilieren
    console.error('Error compiling vertex shader', gl.getShaderInfoLog(vertexShader));
  }
 
+ // Fragment Shader code aus externer Datei laden
+  // Fragment Shader fuer Berechnung der Farbe eines Pixels verantwortlich
  let fragmentTextResponse = await fetch('./fragmentShader.glsl');
  let fragmentText = await fragmentTextResponse.text();
 
- let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
- gl.shaderSource(fragmentShader, fragmentText);
- gl.compileShader(fragmentShader);
- if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+ let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); // Fragment Shader erstellen
+ gl.shaderSource(fragmentShader, fragmentText); // Shader Code in WebGL laden
+ gl.compileShader(fragmentShader); // Shader kompilieren
+ if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){ // pruefen ob Fehler beim kompilieren
    console.error('Error compiling vertex shader', gl.getShaderInfoLog(fragmentShader));
  }
 
- let program = gl.createProgram();
- gl.attachShader(program, vertexShader);
- gl.attachShader(program, fragmentShader);
- gl.linkProgram(program);
+ // Shader Programm erstellen
+ let program = gl.createProgram(); // Shader Programm erstellen
+ gl.attachShader(program, vertexShader); // vertex Shader an Programm knuepfen
+ gl.attachShader(program, fragmentShader); // fragment Shader an Programm knuepfen
+ gl.linkProgram(program); // vertex und fragment Shader verbinden
+ //pruefen ob Programm gueltig ist
  if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
    console.error('Error linking program', gl.getProgramInfoLog(program));
  }
@@ -40,24 +47,24 @@ async function init() {
  if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS)){
    console.error('Error validating program', gl.getProgramInfoLog(program));
  }
-
-  
+ let hex = '#dc3c05';
+ var r = hexToRgb(hex).r/255;
+ var g = hexToRgb(hex).g/255;
+ var b = hexToRgb(hex).b/255;
  
-  var r = 1.0;
-  var g = 0.0;
-  var b = 0.0;
-
+ 
 
   function loop(){
-    
-    r = Math.random();
-    g = Math.random();
-    b = Math.random();
 
+
+    //Hintergrund festlegen
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT); // Framebuffer loeschen und Hintergrund faerben
+
+    //Shader Programm an WebGL zuweisen, um zu nutzen
     gl.useProgram(program);
  
+
     //Strich links
     createTriangle(gl, program, -0.5, 0.5, -0.5, -0.5, -0.4, -0.5, r, g, b); 
     createTriangle(gl, program, -0.5, 0.5, -0.4, 0.5, -0.4, -0.5, r, g, b);
@@ -80,21 +87,29 @@ async function init() {
 
 function createTriangle(gl, program, x1, y1, x2, y2, x3, y3, r, g, b){
 
+  // drei Eckpunkte eines Dreiecks
   let triangleVertices = 
   [ //X  Y
     x1, y1,
     x2, y2,
     x3, y3
   ];
+
+  // Buffer (GPU Speicher) fuer die Dreiecksdaten erstellen
   let triangleVertexBufferObject = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject); // Buffer an GPU Speicher  binden
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW); //Koordinaten des Dreiecks in Buffer schreiben
+  gl.bindBuffer(gl.ARRAY_BUFFER, null); 
 
  
-  // draw
+  // Buffer erneut binden, um Daten zu nutzen
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+
+  // Position des Attributs "vertPosition" aus Shader holen
   let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+
+
+  // WebGL angeben, wie die Vertex-Daten im Buffer gespeichert sind
   gl.vertexAttribPointer(
     positionAttribLocation,
     2,
@@ -103,15 +118,27 @@ function createTriangle(gl, program, x1, y1, x2, y2, x3, y3, r, g, b){
     2 * Float32Array.BYTES_PER_ELEMENT,
     0 * Float32Array.BYTES_PER_ELEMENT
   );
-  gl.enableVertexAttribArray(positionAttribLocation);
+  gl.enableVertexAttribArray(positionAttribLocation); //Attribut 'vertPosition' aktivieren
  
+  // Location der Farbe im Shader holen und auf 'r, g, b' setzen
   let location = gl.getUniformLocation(program, 'color');
   gl.uniform3f(location, r, g, b);
  
+  // Dreieck mit den drei angegebenen Punkten zeichnen
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 
 
 }
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 
 
 window.onload = init;
